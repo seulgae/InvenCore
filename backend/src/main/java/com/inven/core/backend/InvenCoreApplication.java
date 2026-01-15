ae911b4c8af8a2d8ba672316ae3cfbbcfdfe5894
seulgae <r1605866@naver.com>
2026-01-15 16:47:59 +0900

    project upload

---
+++ backend/src/main/java/com/inven/core/backend/InvenCoreApplication.java
@@ -0,0 +1,13 @@
+package com.inven.core.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvenCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvenCoreApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/inven/core/backend/InvenCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inven.core.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvenCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
